5bb9b064755a86a4318c064880e32cbc063fcd53
Linda <poohzoa@gmail.com>
2026-04-24 20:17:02 +1000

    feat: 모바일레이아웃 변경 및 세부내용 수정

--- src/pages/FavoritesPage.jsx
+++ src/pages/FavoritesPage.jsx
@@ -1,26 +1,7 @@
-// 즐겨찾기 페이지 - 저장한 약국 목록 표시
-import PharmacyList from '../components/pharmacy/PharmacyList';
-import EmptyState from '../components/common/EmptyState';
+import React from "react";
 
-function FavoritesPage() {
-  // TODO: usePharmacyStore에서 즐겨찾기 목록 가져오기
-  const favorites = [];
-
-  if (favorites.length === 0) {
-    return (
-      <div className="favorites-page">
-        <h2>즐겨찾기</h2>
-        <EmptyState message="저장한 약국이 없습니다." />
-      </div>
-    );
-  }
-
-  return (
-    <div className="favorites-page">
-      <h2>즐겨찾기</h2>
-      <PharmacyList pharmacies={favorites} onSelect={() => { /* TODO */ }} />
-    </div>
-  );
-}
+const FavoritesPage = () => {
+  return <div>즐겨찾기</div>;
+};
 
 export default FavoritesPage;
